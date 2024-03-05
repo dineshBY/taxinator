84c6db5f0de507f4bc5d8fcc181f3a2cca25b859
Dinesh-mn-20 <dineshmn2003@gmail.com>
2024-03-05 21:56:54 +0530

    changed

--- tax_calculator/settings.py
+++ tax_calculator/settings.py
@@ -50,13 +50,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    "corsheaders",
+    # "corsheaders",
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
-    "corsheaders.middleware.CorsMiddleware",
+    # "corsheaders.middleware.CorsMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -83,9 +83,9 @@ TEMPLATES = [
     },
 ]
 
-CORS_ALLOWED_ORIGINS = [
-    "https://taxinator.onrender.com"
-]
+# CORS_ALLOWED_ORIGINS = [
+#     "https://taxinator.onrender.com"
+# ]
 
 WSGI_APPLICATION = 'tax_calculator.wsgi.application'
 
